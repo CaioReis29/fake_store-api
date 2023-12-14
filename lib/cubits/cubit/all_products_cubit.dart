9f24d7aed084d6373c55b44db1aa20718a0ccf20
CaioReis29 <114621145+CaioReis29@users.noreply.github.com>
2023-12-14 14:07:10 -0300

    feat: added search products function

--- lib/cubits/cubit/all_products_cubit.dart
+++ lib/cubits/cubit/all_products_cubit.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:bloc/bloc.dart';
 import 'package:fake_store_api/data/models/all_products/products.dart';
 import 'package:fake_store_api/data/repositories/products_repository.dart';
+import 'package:flutter/material.dart';
 
 part 'all_products_state.dart';
 
@@ -11,14 +12,34 @@ class AllProductsCubit extends Cubit<AllProductsState> {
 
   ProductsRepository repo = ProductsRepository();
 
+  final TextEditingController searchController = TextEditingController();
+
   Future<void> getAllProducts() async {
     emit(AllProductsLoading());
+    final result = await repo.getAllProducts();
     try {
-      final result = await repo.getAllProducts();
       emit(AllProductsSucess(products: result));
     } catch (e) {
       log(e.toString());
       emit(AllProductsFailure());
+    }
+  }
+
+  Future<void> searchProducts(String value) async {
+    final result = await repo.getAllProducts();
+    if (value.isEmpty) {
+      emit(AllProductsSucess(products: result));
+    } else {
+      final filter = result.where(
+        (product) => product.title!.toLowerCase().contains(
+              value.toLowerCase(),
+            ),
+      );
+      emit(
+        AllProductsSucess(
+          products: filter.toList(),
+        ),
+      );
     }
   }
 }
