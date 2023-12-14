44f36f7a1ee276c3b7c8cae6d82865cea9d6b519
CaioReis29 <114621145+CaioReis29@users.noreply.github.com>
2023-12-14 17:03:39 -0300

    feat: sign in state finished

---
+++ lib/cubits/sign_in_cubit/sign_in_state.dart
@@ -0,0 +1,11 @@
+part of 'sign_in_cubit.dart';
+
+abstract class SignInState {}
+
+final class SignInInitial extends SignInState {}
+
+final class SignInProccess extends SignInState {}
+
+final class SignInFailure extends SignInState {}
+
+final class SignInSucess extends SignInState {}
