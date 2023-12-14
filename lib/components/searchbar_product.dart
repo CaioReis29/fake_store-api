0538bb1d61a26de074d6dbefb32c33c7876efdf7
CaioReis29 <114621145+CaioReis29@users.noreply.github.com>
2023-12-14 15:11:31 -0300

    style: componentized search bar

---
+++ lib/components/searchbar_product.dart
@@ -0,0 +1,54 @@
+import 'package:fake_store_api/cubits/cubit/all_products_cubit.dart';
+import 'package:flutter/material.dart';
+
+class SearchbarProduct extends StatelessWidget {
+  const SearchbarProduct({
+    super.key,
+    required this.cubit,
+  });
+
+  final AllProductsCubit cubit;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 80,
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: TextFormField(
+          controller: cubit.searchController,
+          onChanged: (value) {
+            final searchText = cubit.searchController.text;
+            cubit.searchProducts(searchText);
+          },
+          onFieldSubmitted: (value) =>
+              cubit.searchProducts(cubit.searchController.text),
+          decoration: InputDecoration(
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Theme.of(context).primaryColor,
+              ),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            labelText: 'Search Products',
+            labelStyle: const TextStyle(fontSize: 16),
+            border: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Theme.of(context).primaryColor,
+              ),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Theme.of(context).primaryColor,
+              ),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            floatingLabelBehavior: FloatingLabelBehavior.never,
+          ),
+        ),
+      ),
+    );
+  }
+}
