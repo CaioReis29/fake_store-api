fbb6188c727e4791ac44a24f8ec904a8308a0344
CaioReis29 <114621145+CaioReis29@users.noreply.github.com>
2023-12-14 17:10:09 -0300

    feat: sign in cubit finished

--- lib/cubits/sign_in_cubit/sign_in_cubit.dart
+++ lib/cubits/sign_in_cubit/sign_in_cubit.dart
@@ -1,7 +1,33 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
+import 'package:fake_store_api/data/repositories/auth/sign_in/sign_in_repository.dart';
+import 'package:flutter/material.dart';
 
 part 'sign_in_state.dart';
 
 class SignInCubit extends Cubit<SignInState> {
-  SignInCubit() : super(SignInInitial());
+  SignInCubit(this.repo) : super(SignInInitial());
+  SignInRepository repo;
+
+  GlobalKey<FormState> loginKey = GlobalKey<FormState>();
+
+  final TextEditingController userName = TextEditingController();
+  final TextEditingController password = TextEditingController();
+
+  Future<void> signIn() async {
+    emit(SignInProccess());
+    try {
+      final result =
+          await repo.login(username: userName.text, pass: password.text);
+      if (result) {
+        emit(SignInSucess());
+      } else {
+        emit(SignInFailure());
+      }
+    } catch (e) {
+      log(e.toString());
+      emit(SignInFailure());
+    }
+  }
 }
