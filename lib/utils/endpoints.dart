35cc6184b4f2b12eb922648e876fe1eaf7e679c4
CaioReis29 <114621145+CaioReis29@users.noreply.github.com>
2023-12-15 13:47:50 -0300

    feat: sign up endpoint added

--- lib/utils/endpoints.dart
+++ lib/utils/endpoints.dart
@@ -1,4 +1,5 @@
 class Endpoints {
   static String allProducts = 'https://fakestoreapi.com/products';
   static String loginUrl = 'https://fakestoreapi.com/auth/login';
+  static String signUpUrl = 'https://fakestoreapi.com/users';
 }
