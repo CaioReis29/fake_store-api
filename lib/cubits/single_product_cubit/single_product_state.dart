ab5946118b5146f1f9ac48e70cab9150efc73c89
CaioReis29 <114621145+CaioReis29@users.noreply.github.com>
2023-12-15 13:46:33 -0300

    feat: single product intialized

---
+++ lib/cubits/single_product_cubit/single_product_state.dart
@@ -0,0 +1,11 @@
+part of 'single_product_cubit.dart';
+
+abstract class SingleProductState {}
+
+final class SingleProductInitial extends SingleProductState {}
+
+final class SingleProductFailure extends SingleProductState {}
+
+final class SingleProductLoaging extends SingleProductState {}
+
+final class SingleProductSucess extends SingleProductState {}
