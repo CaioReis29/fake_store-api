6a988dcae29ad5139ffaa63b1ae5389ce05b96ac
CaioReis29 <114621145+CaioReis29@users.noreply.github.com>
2023-12-17 13:19:30 -0300

    style: elevated button style added

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,6 +1,5 @@
 import 'package:fake_store_api/screens/auth/auth_screen.dart';
 import 'package:fake_store_api/screens/home/home_screen.dart';
-import 'package:fake_store_api/screens/single/single_product_screen.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -12,12 +11,27 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Fake Store API',
       theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            elevation: 4,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(7),
+            ),
+            side: BorderSide(
+              color: Theme.of(context).primaryColor,
+            ),
+          ),
+        ),
         brightness: Brightness.dark,
         primaryColor: const Color.fromARGB(255, 243, 208, 113),
         textTheme: const TextTheme(
           bodySmall: TextStyle(
-            fontSize: 15,
-            color: Color.fromARGB(255, 243, 208, 113),
+            fontSize: 17,
+            fontWeight: FontWeight.w500,
+          ),
+          bodyMedium: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
           ),
           titleLarge: TextStyle(
             fontSize: 35,
@@ -35,7 +49,7 @@ class MyApp extends StatelessWidget {
       home: const HomeScreen(),
       routes: {
         '/home': (context) => const HomeScreen(),
-        '/product_details': (context) => const SingleProductScreen(),
+        // '/product_details': (context) => const SingleProductScreen(),
         '/auth_screen': (context) => const AuthScreen(),
       },
     );
