38e3db6dc30f909c93fc3196d2a90b128428389c
CaioReis29 <114621145+CaioReis29@users.noreply.github.com>
2023-12-18 14:23:09 -0300

    feat: categories bloc provider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:fake_store_api/bloc_observer.dart';
+import 'package:fake_store_api/cubits/categories_cubit/all_categories_cubit.dart';
 import 'package:fake_store_api/cubits/products_cubit/all_products_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fake_store_api/app/my_app.dart';
@@ -14,6 +15,9 @@ void main() {
       providers: [
         BlocProvider<AllProductsCubit>(
           create: (context) => AllProductsCubit(),
+        ),
+        BlocProvider<AllCategoriesCubit>(
+          create: (context) => AllCategoriesCubit(),
         ),
       ],
       child: const MyApp(),
