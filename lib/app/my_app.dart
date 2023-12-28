24ae12675f8e86fa3ce8bdc85e1475916bae6fe7
CaioReis29 <114621145+CaioReis29@users.noreply.github.com>
2023-12-28 15:03:24 -0300

    feat: check internet widget added

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity_wrapper/connectivity_wrapper.dart';
 import 'package:fake_store_api/screens/auth/auth_screen.dart';
 import 'package:fake_store_api/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
@@ -9,52 +10,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ScreenUtil.init(context);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Fake Store API',
-      theme: ThemeData(
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            elevation: 4,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(7),
+    return ConnectivityAppWrapper(
+      app: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Fake Store API',
+        theme: ThemeData(
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(7),
+              ),
+              side: const BorderSide(
+                color: Color.fromARGB(255, 243, 208, 113),
+              ),
             ),
-            side: const BorderSide(
-              color: Color.fromARGB(255, 243, 208, 113),
+          ),
+          brightness: Brightness.dark,
+          primaryColor: const Color.fromARGB(255, 243, 208, 113),
+          textTheme: TextTheme(
+            bodySmall: TextStyle(
+              fontSize: 14.sp,
+              fontWeight: FontWeight.bold,
+              color: const Color.fromARGB(255, 243, 208, 113),
+            ),
+            bodyMedium: TextStyle(
+              fontSize: 15.sp,
+              fontWeight: FontWeight.w500,
+            ),
+            titleLarge: TextStyle(
+              fontSize: 30.sp,
+              fontWeight: FontWeight.bold,
+              color: const Color.fromARGB(255, 243, 208, 113),
+            ),
+            titleMedium: TextStyle(
+              fontSize: 25.sp,
+              fontWeight: FontWeight.bold,
+              color: const Color.fromARGB(255, 243, 208, 113),
             ),
           ),
+          useMaterial3: true,
         ),
-        brightness: Brightness.dark,
-        primaryColor: const Color.fromARGB(255, 243, 208, 113),
-        textTheme: TextTheme(
-          bodySmall: TextStyle(
-            fontSize: 14.sp,
-            fontWeight: FontWeight.bold,
-            color: const Color.fromARGB(255, 243, 208, 113),
-          ),
-          bodyMedium: TextStyle(
-            fontSize: 15.sp,
-            fontWeight: FontWeight.w500,
-          ),
-          titleLarge: TextStyle(
-            fontSize: 30.sp,
-            fontWeight: FontWeight.bold,
-            color: const Color.fromARGB(255, 243, 208, 113),
-          ),
-          titleMedium: TextStyle(
-            fontSize: 25.sp,
-            fontWeight: FontWeight.bold,
-            color: const Color.fromARGB(255, 243, 208, 113),
-          ),
-        ),
-        useMaterial3: true,
+        home: const HomeScreen(),
+        routes: {
+          '/home': (context) => const HomeScreen(),
+          // '/product_details': (context) => const SingleProductScreen(),
+          '/auth_screen': (context) => const AuthScreen(),
+        },
       ),
-      home: const HomeScreen(),
-      routes: {
-        '/home': (context) => const HomeScreen(),
-        // '/product_details': (context) => const SingleProductScreen(),
-        '/auth_screen': (context) => const AuthScreen(),
-      },
     );
   }
 }
